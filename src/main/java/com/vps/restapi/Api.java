59b0b69748e6814fd74fa12c9e70b7b03fab4c7c
Filip7656 <Filip7656@users.noreply.github.com>
2018-09-27 11:34:23 +0200

    Cors work

--- src/main/java/com/vps/restapi/Api.java
+++ src/main/java/com/vps/restapi/Api.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +20,6 @@ import com.vps.restapi.model.UserRepository;
 
 //mapowanie rest
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/json")
 public class Api {
 	private static final Logger LOG = LoggerFactory.getLogger(Api.class);
